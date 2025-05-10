24315b4659f8e1fc623c74a762a92277811ce573
Sufian Arshad Khan <103904612+sufianarshadkhan@users.noreply.github.com>
2025-05-10 05:41:59 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Header from './Header'
+import Center from './Center'
+import React, { useState } from 'react'
+
+function App() {
+
+  
+
+  return (
+    <>
+     <Header />
+     <Center />
+     
+      
+    </>
+  )
+}
+
+export default App
